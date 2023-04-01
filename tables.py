93b8956cbfe9c3a9e52a2d301f008420c5f10d87
SozinovD <gamesmail57@gmail.com>
2023-04-01 19:57:56 +0300

    add initial info for db

---
+++ tables.py
@@ -0,0 +1,26 @@
+table_cat = '''CREATE TABLE categories
+              (id INTEGER PRIMARY KEY,
+              type varchar(20) NOT NULL,
+              name varchar(20) NOT NULL)'''
+
+table_curr = '''CREATE TABLE currencies
+              (id INTEGER PRIMARY KEY,
+              name varchar(3) NOT NULL,
+              to_usd REAL)'''
+
+table_rec = '''CREATE TABLE records
+              (id INTEGER PRIMARY KEY,
+              user_id INTEGER NOT NULL,
+              type varchar(20) NOT NULL,
+              category varchar(20) NOT NULL,
+              date_ts INTEGER NOT NULL,
+              comment varchar(100),
+              currency varchar(3) NOT NULL,
+              amount INTEGER NOT NULL)'''
+
+tables_arr = [ table_cat, table_curr, table_rec ]
+
+default_cats_arr = [['Salary', 'income'], ['Sell', 'income'], ['Other', 'income'], \
+                    ['Food', 'expense'], ['Clothes', 'expense'],  ['Fun', 'expense'], ['Other', 'expense']]
+
+default_currs_arr = ['RUR', 'KZT', 'DKK', 'RUR']
