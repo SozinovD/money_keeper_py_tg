5e1cbfce708679c0226eee97837927b9439d1106
SozinovD <gamesmail57@gmail.com>
2023-04-03 21:37:40 +0300

    add simple way to get today's currencies rates

---
+++ currency_api_handler.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# this is just genious
+# https://github.com/fawazahmed0/currency-api
+
+import requests
+
+base_urls_arr = []
+base_urls_arr.append('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/')
+base_urls_arr.append('https://raw.githubusercontent.com/fawazahmed0/currency-api/1/')
+
+suffix_arr = []
+suffix_arr.append('.min.json')
+suffix_arr.append('.json')
+
+def get_rate(curr1, curr2, date):
+  curr1 = curr1.lower()
+  curr2 = curr2.lower()
+  for base_url in base_urls_arr:
+    for suff in suffix_arr:
+      url = base_url + date + '/currencies/' + curr1 + '/' + curr2 + suff
+      rate = requests.get(url)
+      if rate.status_code == 200:
+        break
+    if rate.status_code == 200:
+      break
+
+  return rate.json()[curr2]
+
+def get_today_rate(curr1, curr2):
+  return get_rate(curr1, curr2, 'latest')
