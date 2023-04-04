2cad0bd4ad8fcf738975f60392bb0c85f1d5dae7
SozinovD <gamesmail57@gmail.com>
2023-04-04 18:52:39 +0300

    actually it doesn't work, State Machine needs to be implemented

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 
 import time
 
-# import telebot
-# from telebot import types
-
-from config import api_key as TOKEN
+from config import bot_token
 from config import owner_id
 from config import db_filename
 
@@ -18,11 +15,10 @@ from aiogram import Bot, types
 from aiogram.utils import executor
 from aiogram.utils.markdown import text
 from aiogram.dispatcher import Dispatcher
-bot = Bot(TOKEN)
+
+bot = Bot(bot_token)
 dp = Dispatcher(bot)
 
-
-# bot = telebot.TeleBot(api_key)
 data_divider_in_callback = '\''
 
 # I tried to use global record, but it simply didn't work
@@ -62,7 +58,7 @@ async def finalise_new_record(message):
   line = db.add_rec(db_filename, new_rec_glob[0])
   if not line:
     line = 'Record added'
-  bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line)
+  await bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line)
 
 async def add_curr_handler(message):
   curr_name = message.text
@@ -112,72 +108,74 @@ async def start(message):
       print(record)
       await bot.send_message(message.from_user.id, str(record.__dict__))
 
-# @dp.callback_query_handler(func=lambda call: True)
-# async def callback_inline(callback_query: types.CallbackQuery):
-#   key = types.InlineKeyboardMarkup()
-#   line = ''
-#   message = call.message
-#   new_rec_glob.insert(1, message.chat.id)
-#   new_rec_glob.insert(2, message.message_id)
+# @dp.callback_query_handler(lambda c: c.data and c.data.startswith('start'))
+@dp.callback_query_handler(lambda c: c.data)
+async def callback_inline(call: types.CallbackQuery):
+  key = types.InlineKeyboardMarkup()
+  line = ''
+  message = call.message
+  new_rec_glob.insert(1, message.chat.id)
+  new_rec_glob.insert(2, message.message_id)
 
-#   data_arr = funcs.dec_callback_data(data_divider_in_callback, call.data)
-#   data_marker = data_arr[0]
-#   data_body_arr = []
-#   for data in data_arr[1:]:
-#     data_body_arr.append(data)
+  data_arr = funcs.dec_callback_data(data_divider_in_callback, call.data)
+  data_marker = data_arr[0]
+  data_body_arr = []
+  for data in data_arr[1:]:
+    data_body_arr.append(data)
 
-#   print('data_marker', data_marker)
-#   print('data_body_arr', data_body_arr)
-#   # print(data_body_arr)
+  # print('data_marker', data_marker)
+  # print('data_body_arr', data_body_arr)
+
 
 #   # part for /add_record START
-#   if data_marker == 'back_to_start':
-#     line = 'Choose type of record'
-#     key = funcs.get_start_rec_add_kbrd(data_divider_in_callback)
-#     bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line, reply_markup=key)
+  if data_marker == 'back_to_start':
+    line = 'Choose type of record'
+    key = funcs.get_start_rec_add_kbrd(data_divider_in_callback)
+    await bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line, reply_markup=key)
 
-#   if data_marker == 'start':
-#     line = 'Choose category'
-#     key = funcs.get_cat_btns_by_type(db_filename, data_body_arr[0], data_divider_in_callback)
-#     new_rec_glob[0].set_currency(db.get_last_rec_currency(db_filename, data_body_arr[1]))
-#     bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line, reply_markup=key)
+  if data_marker == 'start':
+    line = 'Choose category'
+    key = funcs.get_cat_btns_by_type(db_filename, data_body_arr[0], data_divider_in_callback)
+    new_rec_glob[0].set_currency(db.get_last_rec_currency(db_filename, data_body_arr[1]))
+    await bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line, reply_markup=key)
 
-#   if data_marker == 'income' or data_marker == 'expense' or data_marker == 'a_r_curr':
-#     key = funcs.get_currency_btns(db_filename, data_divider_in_callback, 'a_r_')
-#     back_btn_callback_data = funcs.enc_callback_data(data_divider_in_callback, 'back_to_start\'')
-#     key.add(types.InlineKeyboardButton(text='Back to start', callback_data=back_btn_callback_data))
+  if data_marker == 'income' or data_marker == 'expense' or data_marker == 'a_r_curr':
+    key = funcs.get_currency_btns(db_filename, data_divider_in_callback, 'a_r_')
+    back_btn_callback_data = funcs.enc_callback_data(data_divider_in_callback, 'back_to_start\'')
+    key.add(types.InlineKeyboardButton(text='Back to start', callback_data=back_btn_callback_data))
     
-#     if data_marker == 'a_r_curr':
-#       new_rec_glob[0].set_currency(data_body_arr[0])
-#     else:
-#       new_rec_glob[0].set_category(classes.Category(data_marker, data_body_arr[0]))
+    if data_marker == 'a_r_curr':
+      new_rec_glob[0].set_currency(data_body_arr[0])
+    else:
+      new_rec_glob[0].set_category(classes.Category(data_marker, data_body_arr[0]))
 
-#     line = 'Input record info, example:\n' \
-#            '`1234 really great bananas!\n`' \
-#            'Means \'I just spent (or earned) 1234 ' + new_rec_glob[0].currency + ' on bananas that i really love\''
-#     try:
-#       bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line, parse_mode='Markdown', reply_markup=key)
-#     except Exception as e:
-#       print(e)
+    line = '*' + new_rec_glob[0].currency + ' = ' + str(currs_api.get_today_rate(new_rec_glob[0].currency, 'usd')) + ' USD' \
+          '*\nInput record info, example:\n' \
+          '`1200 really great bananas!\n`' \
+          'Means \n\'_I just spent (or earned) 1200 on bananas that i really love_\''
+    try:
+      await bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line, parse_mode='Markdown', reply_markup=key)
+    except Exception as e:
+      print(e)
 
-#     if data_marker != 'a_r_curr':
-#       bot.register_next_step_handler(message, finalise_new_record)
-#   # part for /add_record END
+    if data_marker != 'a_r_curr':
+      bot.register_next_step_handler(message, finalise_new_record)
+  # part for /add_record END
 
-#   if data_marker == 'curr_setup':
-#     if data_body_arr[0] == 'add':
-#       line = 'Input currency identificator, for example\nUSD'
-#       bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line)
-#       bot.register_next_step_handler(message, add_curr_handler)
-#     else:
-#       key = funcs.get_currency_btns(db_filename, data_divider_in_callback, 'del_')
-#       line = 'Choose currency to delete'
-#       bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line, reply_markup=key)
+  if data_marker == 'curr_setup':
+    if data_body_arr[0] == 'add':
+      line = 'Input currency identificator, for example\nUSD'
+      await bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line)
+      bot.register_next_step_handler(message, add_curr_handler)
+    else:
+      key = funcs.get_currency_btns(db_filename, data_divider_in_callback, 'del_')
+      line = 'Choose currency to delete'
+      await bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line, reply_markup=key)
 
-#   if data_marker == 'del_curr':
-#     # print('del curr', data_body_arr[0])
-#     line = db.del_curr(db_filename, data_body_arr[0])
-#     bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line)
+  if data_marker == 'del_curr':
+    # print('del curr', data_body_arr[0])
+    line = db.del_curr(db_filename, data_body_arr[0])
+    await bot.edit_message_text(chat_id=new_rec_glob[1], message_id=new_rec_glob[2], text=line)
 
 
 if __name__ == '__main__':
@@ -186,5 +184,3 @@ if __name__ == '__main__':
   print('Start db:', db_started)
 
   executor.start_polling(dp)
-
-  # bot.infinity_polling()
